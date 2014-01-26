d104276474767ef184dd44581bd9646833ce3896
Nodcah <Nodcah@192.168.1.66>
2014-01-26 12:42:50 -0500

    Merge origin/master
    Conflicts:
    	src/edu/team3182/main/Robot3182.java

--- src/edu/team3182/main/Robot3182.java
+++ src/edu/team3182/main/Robot3182.java
@@ -28,15 +28,15 @@ public class Robot3182 extends IterativeRobot {
      * used for any initialization code.
      */
     private RobotDrive drive;
-    private Joystick rightjoystick;
-    private Joystick leftjoystick;
-    private Joystick buttonsjoystick;
-    private Talon shootermotors;
-    private Talon collectormotor;
-    private Solenoid leftshifter;
-    private Solenoid rightshifter;
-    private Solenoid leftcollector;
-    private Solenoid rightcollector;
+    private Joystick rightJoystick;
+    private Joystick leftJoystick;
+    private Joystick buttonsJoystick;
+    private Talon shooterMotors;
+    private Talon collectorMotor;
+    private Solenoid leftShifter;
+    private Solenoid rightShifter;
+    private Solenoid leftCollector;
+    private Solenoid rightCollector;
 
     /**
      * Called when the robot is first turned on. This is a substitute for using
@@ -45,15 +45,15 @@ public class Robot3182 extends IterativeRobot {
      */
     public void robotInit() {
         drive = new RobotDrive(1, 2);
-        rightjoystick = new Joystick(1);
-        leftjoystick = new Joystick(2);
-        buttonsjoystick = new Joystick(3);
-        shootermotors = new Talon(4);
-        collectormotor = new Talon(3);
-        leftshifter = new Solenoid(5, 6);
-        rightshifter = new Solenoid(7, 8);
-        leftcollector = new Solenoid(1, 2);
-        rightcollector = new Solenoid(3, 4);
+        rightJoystick = new Joystick(1);
+        leftJoystick = new Joystick(2);
+        buttonsJoystick = new Joystick(3);
+        shooterMotors = new Talon(4);
+        collectorMotor = new Talon(3);
+        leftShifter = new Solenoid(5, 6);
+        rightShifter = new Solenoid(7, 8);
+        leftCollector = new Solenoid(1, 2);
+        rightCollector = new Solenoid(3, 4);
 
     }
 
@@ -66,17 +66,17 @@ public class Robot3182 extends IterativeRobot {
         //Drive forward for 2 seconds
         drive.drive(0.3, 0.0);
         Timer.delay(2.0);
-        drive.drive(0.0, 0.0)
+        drive.drive(0.0, 0.0);
                 
         //Shoot:
         //Move motors forward and wait for the ball to be shot
-        shootermotors.set(1.0);
+        shooterMotors.set(1.0);
         Timer.delay(2);
         //Reload
-        shootermotors.set(-1.0);
+        shooterMotors.set(-1.0);
         Timer.delay(2.0);
         //Stop 
-        shootermotors.set(0.0);
+        shooterMotors.set(0.0);
         
        
     }
@@ -115,8 +115,8 @@ public class Robot3182 extends IterativeRobot {
         boolean reverseshooter;
         
         // sets xAxisRight and xAxisLeft to the x axis of corresponding joysticks
-        xAxisRight = rightjoystick.getAxis(Joystick.AxisType.kX);
-        xAxisLeft = leftjoystick.getAxis(Joystick.AxisType.kX);
+        xAxisRight = rightJoystick.getAxis(Joystick.AxisType.kX);
+        xAxisLeft = leftJoystick.getAxis(Joystick.AxisType.kX);
         
         // makes sure joystick will not work at +-25%
         if ((xAxisRight < .25 && xAxisRight > (-.25))) {
@@ -129,23 +129,23 @@ public class Robot3182 extends IterativeRobot {
         drive.tankDrive(xAxisRight, xAxisLeft);
         
         // shoot is button 1, revershooter is button 2 
-        shoot = buttonsjoystick.getRawButton(1);
-        reverseshooter = buttonsjoystick.getRawButton(2);
+        shoot = buttonsJoystick.getRawButton(1);
+        reverseshooter = buttonsJoystick.getRawButton(2);
         
         /* When button 1 is pressed, set the motors to 70%
          * When button 2 is pressed, set motors to reverse at 50% for 1 seconds
         */
         if (shoot == true) {
-            shootermotors.set(.7);
+            shooterMotors.set(.7);
         } else if (shoot == false) {
-            shootermotors.set(0);
+            shooterMotors.set(0);
         }
         if (reverseshooter == true) {
-            shootermotors.set(-.5);
+            shooterMotors.set(-.5);
             Timer.delay(1);
-            shootermotors.set(0);
+            shooterMotors.set(0);
         } else if (reverseshooter == false) {
-            shootermotors.set(0);
+            shooterMotors.set(0);
         }
     }
 
