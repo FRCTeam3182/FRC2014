8b2aa44947db9ec867c4fdaa7528c00cea46a863
Team3182 <Team3182@Team3182-HP>
2014-01-28 19:34:45 -0500

    Added basic code to run the collector

--- src/edu/team3182/main/Robot3182.java
+++ src/edu/team3182/main/Robot3182.java
@@ -6,6 +6,7 @@
 /*----------------------------------------------------------------------------*/
 package edu.team3182.main;
 
+import edu.wpi.first.wpilibj.DigitalModule;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -13,6 +14,7 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.I2C;
 //import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -41,7 +43,7 @@ public class Robot3182 extends IterativeRobot {
     private Encoder driveEncoder;
     private I2C ledStrip;
     private DigitalModule ledStripDigitalModule;
-    
+
     /**
      * Called when the robot is first turned on. This is a substitute for using
      * the constructor in the class for consistency. This method is only called
@@ -55,13 +57,10 @@ public class Robot3182 extends IterativeRobot {
         buttonsJoystick = new Joystick(3);
         shooterMotors = new Talon(4);
         collectorMotor = new Talon(3);
-        //leftShifter = new Solenoid(5, 6);
-        //rightShifter = new Solenoid(7, 8);
-        //leftCollector = new Solenoid(1, 2);
-        //rightCollector = new Solenoid(3, 4);
-        
-      
-       
+        leftShifter = new Solenoid(2, 6);
+        rightShifter = new Solenoid(2, 8);
+        leftCollector = new Solenoid(2, 2);
+        rightCollector = new Solenoid(2, 4);
 
     }
 
@@ -72,33 +71,31 @@ public class Robot3182 extends IterativeRobot {
     public void autonomousInit() {
         //Send command to Arduino for the light strip
         ledStrip.write(4, 188);
-        
+
         //Drive forward for 2 seconds
-          drive.drive(0.3, 0.0);
+        drive.drive(0.3, 0.0);
         Timer.delay(2.0);
         drive.drive(0.0, 0.0);
-                
+
         //Shoot:
         //Move motors forward and wait for the ball to be shot
-         shooterMotors.set(1.0);
+        shooterMotors.set(1.0);
         Timer.delay(2);
         //Reload
         shooterMotors.set(-1.0);
         Timer.delay(2.0);
         //Stop 
         shooterMotors.set(0.0);
-        
-       
+
     }
-    
+
     /**
      * Called when the robot enters the teleop period for the first time. This
      * is called on a transition from any other state.
      */
     public void teleopInit() {
-       
-    }
 
+    }
 
     /**
      * This function is called periodically during operator control
@@ -107,51 +104,66 @@ public class Robot3182 extends IterativeRobot {
         double yAxisRight;
         double yAxisLeft;
         boolean shoot;
-        boolean reverseshooter;
-        
-         //sets yAxisRight and yAxisLeft to the x axis of corresponding joysticks
+        boolean reverseShooter;
+        boolean collect;
+        boolean collectReverse;
+
+        //sets yAxisRight and yAxisLeft to the x axis of corresponding joysticks
         yAxisRight = rightJoystick.getAxis(Joystick.AxisType.kY);
         yAxisLeft = leftJoystick.getAxis(Joystick.AxisType.kY);
-        
-         //makes sure joystick will not work at +-25%
-       if ((yAxisRight < .25 && yAxisRight > (-.25))) {
-           yAxisRight = 0;
-       }
-       if (yAxisLeft < .25 && yAxisLeft > (-.25)) {
-          yAxisLeft = 0;
-      }
+
+        //makes sure joystick will not work at +-25%
+        if ((yAxisRight < .25 && yAxisRight > (-.25))) {
+            yAxisRight = 0;
+        }
+        if (yAxisLeft < .25 && yAxisLeft > (-.25)) {
+            yAxisLeft = 0;
+        }
          //drive using the joysticks
-        
+
         drive.tankDrive(yAxisRight, yAxisLeft);
-        
-         //shoot is button 1, revershooter is button 2 
+
+        //shoot is button 1, revershooter is button 2 
         shoot = buttonsJoystick.getRawButton(1);
-        reverseshooter = buttonsJoystick.getRawButton(2);
-        
+        reverseShooter = buttonsJoystick.getRawButton(2);
+        collect = buttonsJoystick.getRawButton(3);
+        collectReverse = buttonsJoystick.getRawButton(4);
+
         // When button 1 is pressed, set the motors to 70%
         // When button 2 is pressed, set motors to reverse at 50% for 1 seconds
-        
         if (shoot == true) {
             shooterMotors.set(.7);
         } else if (shoot == false) {
             shooterMotors.set(0);
         }
-        if (reverseshooter == true) {
+        if (reverseShooter == true) {
             shooterMotors.set(-.5);
             Timer.delay(1);
             shooterMotors.set(0);
-        } else if (reverseshooter == false) {
+        } else if (reverseShooter == false) {
             shooterMotors.set(0);
-        
+
         }
-        // SmartDashboard.putNumber("Speed", shooterMotors.getSpeed());
+        // if button 3 is pressed, run the collector motor at 90%
+        // if button 4 is pressed, run the collector motor in reverse at 90%
+        if (collect == true) {
+            collectorMotor.set(.9);
+        } else if (collect == false) {
+            collectorMotor.set(0);
+        }
+        if (collectReverse == true) {
+            collectorMotor.set(-.9);
+        } else if (collectReverse == false) {
+            collectorMotor.set(0);
+        }
+
     }
+    // SmartDashboard.putNumber("Speed", shooterMotors.getSpeed());
 
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-        
 
     }
 
