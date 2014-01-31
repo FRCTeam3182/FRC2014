8e3327e7d607139b5dc4f1b85c943aa123d584b7
Team3182 <Team3182@192.168.1.57>
2014-01-30 20:51:39 -0500

    Check smoothVar if statements! The equation is weird

--- src/edu/team3182/main/Robot3182.java
+++ src/edu/team3182/main/Robot3182.java
@@ -39,6 +39,17 @@ public class Robot3182 extends IterativeRobot {
     private Solenoid leftCollector;
     private Solenoid rightCollector;
     private Encoder driveEncoder;
+    double yAxisRight;
+    double yAxisLeft;
+    boolean shoot;
+    boolean airPass;
+    boolean reverseShooter;
+    boolean collect;
+    boolean collectReverse;
+    boolean collectorFoward;
+    double p = 0.25;
+    double smoothVarRight = 0;
+    double smoothVarLeft = 0;
 
     /**
      * Called when the robot is first turned on. This is a substitute for using
@@ -74,8 +85,8 @@ public class Robot3182 extends IterativeRobot {
 
         //Shoot:
         //quickly speed up motors, then wait for the ball to be shot
-        for (int i=1; i<=10; i++){ //takes half a second to reach full speed
-            shooterMotors.set(i/10);
+        for (int i = 1; i <= 10; i++) { //takes half a second to reach full speed
+            shooterMotors.set(i / 10);
             Timer.delay(.05);
         }
         shooterMotors.set(1);
@@ -86,7 +97,8 @@ public class Robot3182 extends IterativeRobot {
         Timer.delay(2);
         shooterMotors.set(0);
     }
-    public void autonomousPeriodic(){
+
+    public void autonomousPeriodic() {
         Timer.delay(.01);
     }
 
@@ -102,50 +114,49 @@ public class Robot3182 extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        double yAxisRight;
-        double yAxisLeft;
-        boolean shoot;
-        boolean airPass;
-        boolean reverseShooter;
-        boolean collect;
-        boolean collectReverse;
-        boolean collectorFoward;
-        double p = 0.25;
-        double smoothVarRight = 0;
-        double smoothVarLeft = 0;
 
         //sets yAxisRight and yAxisLeft to the x axis of corresponding joysticks
         yAxisRight = rightJoystick.getAxis(Joystick.AxisType.kY);
         yAxisLeft = leftJoystick.getAxis(Joystick.AxisType.kY);
 
+        System.out.println("Right: " + yAxisRight + " Left: " + yAxisLeft);
+
         //makes sure joystick will not work at +-25%
         if (yAxisRight < p && yAxisRight > (-p)) {
-            yAxisRight = 0;
+            System.out.println("1234");
+            smoothVarRight = 0;
         }
         if (yAxisLeft < p && yAxisLeft > (-p)) {
-            yAxisLeft = 0;
+            System.out.println("left1234");
+            smoothVarLeft = 0;
         }
         //smooth left joystick
         //positive
-        if (yAxisLeft >= p){
-            smoothVarLeft = ((1/(1-p))*yAxisLeft+(1-(1/(1-p))));
+        if (yAxisLeft >= p) {
+            smoothVarLeft = ((1 / (1 - p)) * yAxisLeft + (1 - (1 / (1 - p))));
+            System.out.println("SMOOTHVARLEFT!! IF >P: " + smoothVarLeft);
         }
         //negative
-        if (yAxisLeft <= p){
-            smoothVarLeft = ((1/(1-p))*yAxisLeft-(1+(1/(1-p))));
+        if (yAxisLeft <= -p) {
+            smoothVarLeft = ((1 / (1 - p)) * yAxisLeft - (1 + (1 / (1 - p))));
+            System.out.println("SMOOTHVARLEFT!! IF < P: " + smoothVarLeft);
         }
         //smooth right joystick
         //positive
-        if (yAxisRight >= p){
-            smoothVarRight = ((1/(1-p))*yAxisRight+(1-(1/(1-p))));
+        if (yAxisRight >= p) {
+            smoothVarRight = ((1 / (1 - p)) * yAxisRight + (1 - (1 / (1 - p))));
+            System.out.println("SMOOTHVARRIGHT!: IF > P: " + smoothVarRight);
         }
         //negative
-        if (yAxisRight >= p){
-            smoothVarRight = ((1/(1-p))*yAxisRight-(1+(1/(1-p))));
+        if (yAxisRight <= -p) {
+            smoothVarRight = ((1 / (1 - p)) * yAxisRight - (1 + (1 / (1 - p))));
+             System.out.println("SMOOTHVARRIGHT!: IF < P: " + smoothVarRight);
         }
-        
+        System.out.println(p);
+
+        System.out.println("SmoothVarRight: " + smoothVarRight + "SmoothVarLeft: " + smoothVarLeft);
         //drive using the joysticks
-        drive.tankDrive(smoothVarRight, smoothVarLeft);
+        drive.tankDrive(smoothVarLeft, smoothVarRight);
 
         //shoot is button 1, air pass is button 2, collect is 3, ground pass/dump is 4
         shoot = buttonsJoystick.getRawButton(1);
@@ -155,92 +166,90 @@ public class Robot3182 extends IterativeRobot {
 
         // When button 1 is pressed, set the motors to 70% for 1 second
         // When button 2 is pressed, set motors to reverse at 50% for 1 seconds
-        if (shoot == true) {
-            for (int i=1; i<=10; i++){ //takes half a second to reach full speed
-                shooterMotors.set(i/10);
-                Timer.delay(.05);
-            }
-            shooterMotors.set(1);
-            Timer.delay(1);
-            shooterMotors.set(0);
-            Timer.delay(1);
-            shooterMotors.set(-.3);
-            Timer.delay(2);
-            shooterMotors.set(0);
-        }
-        else if (shoot == false) {
-            shooterMotors.set(0);
-        }
-        if (airPass == true){
-            for (int i=1; i<=5; i++){//takes half a second to reach half speed
-                shooterMotors.set(i/10);
-                Timer.delay(.1);
-            }
-            shooterMotors.set(.5);
-            Timer.delay(1);
-            shooterMotors.set(0);
-            Timer.delay(1);
-            shooterMotors.set(-.3);
-            Timer.delay(2);
-            shooterMotors.set(0);
-        }
-        else if (airPass == true){
-            shooterMotors.set(0);
-        }
-        
-        // if button 3 is pressed, run the collector motor at 90%
-        // if button 4 is pressed, run the collector motor in reverse at 90%
-        if (collect == true) {
-            collectorMotor.set(.9);
-        } else if (collect == false) {
-            collectorMotor.set(0);
-        }
-        if (collectReverse == true) {
-            collectorMotor.set(-.9);
-        } else if (collectReverse == false) {
-            collectorMotor.set(0);
-        }
-        //Maneuvers: (trigger on left is half turn, trigger on right is quarter turn)
-        boolean halfTurn = leftJoystick.getRawButton(1);
-        boolean quarterTurn = rightJoystick.getRawButton(1);
-        
-        //turns around quickly 
-        if (halfTurn = true){
-            for (int i=1; i<=10; i++){ //takes half a second to reach full speed
-                drive.drive(0,(i/10));
-                Timer.delay(.05);
-            }
-            drive.drive(0, 1);
-            Timer.delay(1);
-            drive.drive(0, 0);
-        }
-        //does a quarter turn quickly
-        if (quarterTurn = true){
-            for (int i=0; i<=100; i++){
-                drive.drive(0,(i/100));
-                Timer.delay(.02);
-            }
-            drive.drive(0, 1);
-            Timer.delay(.5);
-            drive.drive(0, 0);
-        }
-        
-    } 
+//        if (shoot == true) {
+//////            for (int i = 1; i <= 10; i++) { //takes half a second to reach full speed
+//                shooterMotors.set(i / 10);
+//                Timer.delay(.05);
+//            }
+//            shooterMotors.set(1);
+//            Timer.delay(1);
+//            shooterMotors.set(0);
+//            Timer.delay(1);
+//            shooterMotors.set(-.3);
+//            Timer.delay(2);
+//            shooterMotors.set(0);
+//        } else if (shoot == false) {
+//            shooterMotors.set(0);
+//        }
+//        if (airPass == true) {
+//            for (int i = 1; i <= 5; i++) {//takes half a second to reach half speed
+//                shooterMotors.set(i / 10);
+//                Timer.delay(.1);
+//            }
+//            shooterMotors.set(.5);
+//            Timer.delay(1);
+//            shooterMotors.set(0);
+//            Timer.delay(1);
+//            shooterMotors.set(-.3);
+//            Timer.delay(2);
+//            shooterMotors.set(0);
+//        } else if (airPass == true) {
+//            shooterMotors.set(0);
+//        }
+//
+//        // if button 3 is pressed, run the collector motor at 90%
+//        // if button 4 is pressed, run the collector motor in reverse at 90%
+//        if (collect == true) {
+//            collectorMotor.set(.9);
+//        } else if (collect == false) {
+//            collectorMotor.set(0);
+//        }
+//        if (collectReverse == true) {
+//            collectorMotor.set(-.9);
+//        } else if (collectReverse == false) {
+//            collectorMotor.set(0);
+//        }
+//        //Maneuvers: (trigger on left is half turn, trigger on right is quarter turn)
+//        boolean halfTurn = leftJoystick.getRawButton(1);
+//        boolean quarterTurn = rightJoystick.getRawButton(1);
+//
+//        //turns around quickly 
+//        if (halfTurn = true) {
+//            for (int i = 1; i <= 10; i++) { //takes half a second to reach full speed
+//                drive.drive(0, (i / 10));
+//                Timer.delay(.05);
+//            }
+//            drive.drive(0, 1);
+//            Timer.delay(1);
+//            drive.drive(0, 0);
+//        }
+//        //does a quarter turn quickly
+//        if (quarterTurn = true) {
+//            for (int i = 0; i <= 100; i++) {
+//                drive.drive(0, (i / 100));
+//                Timer.delay(.02);
+//            }
+//            drive.drive(0, 1);
+//            Timer.delay(.5);
+//            drive.drive(0, 0);
+//        }
+
+    }
     // SmartDashboard.putNumber("Speed", shooterMotors.getSpeed());
-    
-    public void disabledInit(){
-    
+
+    public void disabledInit() {
+
     }
-    
-    public void disabledPeriodic(){
-        
+
+    public void disabledPeriodic() {
+
     }
-    
+
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
 
     }
-    
+
 }
