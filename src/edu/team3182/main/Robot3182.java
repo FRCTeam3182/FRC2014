51fbd4a9abf719a8a8f309aedcc16e51eb99f6ba
Peter766 <peter7661211@gmail.com>
2014-03-22 14:10:26 -0400

    Input a value in the dashboard to create a delay in auto

--- src/edu/team3182/main/Robot3182.java
+++ src/edu/team3182/main/Robot3182.java
@@ -61,8 +61,10 @@ public class Robot3182 extends IterativeRobot {
     public void autonomousInit() {
         //disable joystick command over the wheels
         driveTrainVar.setJoystickStateCommand(false);
-
+        double delayAuto;
+        delayAuto = SmartDashboard.getNumber("AUTO DELAY TIME");
         //drive forward
+        Timer.delay(delayAuto);
         driveTrainVar.setRightMotorCommand(-.71);
         driveTrainVar.setLeftMotorCommand(-.7);
         Timer.delay(1.7);
